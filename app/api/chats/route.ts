1cb69be174a66b2c72de8a92ad09c26e1c52ba41
Elvis Rugamba <elvisrugamba@gmail.com>
2024-03-28 14:40:23 +0200

    Add api query params validations

--- app/api/chats/route.ts
+++ app/api/chats/route.ts
@@ -1,10 +1,32 @@
+import { z } from "zod"
+
 import prisma from "@/lib/prisma"
 
 type Sort = "asc" | "desc" | undefined
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const sort: Sort = searchParams.get("sort") as Sort
+
+  const schema = z.object({
+    sort: z.enum(["asc", "desc"]).optional(),
+  })
+
+  const result = schema.safeParse({ sort })
+  if (!result.success) {
+    const { errors } = result.error
+    return new Response(
+      JSON.stringify({
+        status: 400,
+        message: errors.map((error) => error.message).join(", "),
+      }),
+      {
+        status: 400,
+      }
+    )
+  }
 
   const chats = await prisma.chat.findMany({
     orderBy: { createdAt: sort || "desc" },
